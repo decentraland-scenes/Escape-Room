9e38f65c943ba2f657fa023ec8a9228e92c9b0d5
pbosio <bosio.patricio@pbosio.com>
2019-05-16 17:31:12 -0300

    update on room4

--- src/game.ts
+++ src/game.ts
@@ -6,6 +6,7 @@ import { CreateRoom3 } from "./room3";
 import { CreateRoom6 } from "./room6";
 import { CreateRoom7 } from "./room7";
 import { CreateRoom8 } from "./room8";
+import { CreateRoom4 } from "./room4";
 
 const scene = new Entity()
 scene.addComponent(new GLTFShape("models/scene.glb"))
@@ -19,7 +20,7 @@ CreateRoom0()
 CreateRoom1()
 CreateRoom2()
 CreateRoom3()
-
+CreateRoom4()
 
 CreateRoom6()
 CreateRoom7()
--- src/room4.ts
+++ src/room4.ts
@@ -2,65 +2,104 @@ import { ToggleComponent } from "./modules/toggleComponent";
 import { RotateTransformComponent } from "./modules/transfromSystem";
 
 export function CreateRoom4() : void{
-    const panelCanvas = new UICanvas()
-    panelCanvas.visible = false
-    const panelPosition = new Vector2(0, 0)
+    //create canvas for UI elements
+    const uiCanvas = new UICanvas()
+    uiCanvas.visible = false
 
+    //create rect to contain numerical pad
+    const panelRect = new UIContainerRect(uiCanvas)
+    panelRect.positionX = -50
+    panelRect.positionY = 50
+    panelRect.width = "100%"
+    panelRect.height = "100%"
+    panelRect.visible = false
+
+    //create texture for buttons
     const buttonTexture = new Texture("images/room4/pwdpanel_buttons.png")
     const inputTexture = new Texture("images/room4/pwdpanel_input.png")
     const closeTexture = new Texture("images/room4/button_close.png")
 
-    let currentInputIdx = 0
-    let password = "155"
+    //background for numerical pad
+    const panelBg = new UIImage(panelRect, new Texture("images/room4/pwdpanel_bg.png"))
+    panelBg.sourceWidth = 222
+    panelBg.sourceHeight = 297
+    panelBg.width = 310
+    panelBg.height = 420
+    panelBg.positionX = 70
+    panelBg.positionY = -55
 
-    const buttonSize = new Vector2(64, 64)
-    const buttonSpace = new Vector2(5, 5)
-
-    let panelCloseButton = new UIImage(panelCanvas, closeTexture)
+    //close button for numerical pad
+    let panelCloseButton = new UIImage(panelRect, closeTexture)
     panelCloseButton.sourceWidth = 32
     panelCloseButton.sourceHeight = 32
-    panelCloseButton.positionX = panelPosition.x + 3 * (buttonSpace.x + buttonSize.x)
-    panelCloseButton.positionY = panelPosition.y + 2 * (buttonSpace.y + buttonSize.y)
+    panelCloseButton.positionX = 204
+    panelCloseButton.positionY = 133
     panelCloseButton.width = 32
     panelCloseButton.height = 32
     panelCloseButton.onClick = new OnClick(event=>{
-        panelCanvas.visible = false
+        panelRect.visible = false
+        uiCanvas.visible = false
     })
 
-    let panelText = new UIText(panelCanvas)
-    panelText.positionY = panelPosition.y + 2 * (buttonSpace.y + buttonSize.y)
-    panelText.positionX = panelPosition.x + 16
+    //text dor numerical pad
+    let panelText = new UIText(panelRect)
+    panelText.positionY = 133
+    panelText.positionX = 16
     panelText.hTextAlign = "left"
     panelText.vTextAlign = "center"
     panelText.fontSize = 30
     panelText.value = "Enter Code"
+    panelText.isPointerBlocker = false
 
+    //set position offset for buttons
+    const panelPosition = new Vector2(0, -12)
+    //set buttons size
+    const buttonSize = new Vector2(64, 64)
+    //set space between buttons
+    const buttonSpace = new Vector2(5, 5)
+
+    //set current digit index and password value
+    let currentInputIdx = 0
+    let password = "155"
+
+    //create buttons for numerical path
     for (let col=0;col<3;col++){
         for (let row=0; row<4; row++){
+            //references for button image and number text
             let buttonImage: UIImage = null
             let numberText: UIText = null
+            //value of the number
             let value = (row*3 + col) + 1
 
+            //if it is in the last row in the middle we hardcode value to 0
             if (col == 1 && row == 3){
                 value = 0
             }
             
+            //at the bottom left we have the "clear" button
             if (col == 0 && row == 3){
-                buttonImage = new UIImage(panelCanvas, new Texture("images/room4/pwdpanel_clear.png"))
+                //create image for button
+                buttonImage = new UIImage(panelRect, new Texture("images/room4/pwdpanel_clear.png"))
+                //when clicked we reset text values and set digit index to 0
                 buttonImage.onClick = new OnClick(event =>{
                     panelInputs.forEach(inputSlot => {
                         inputSlot.text.value = ""
+                        inputSlot.text.color = Color4.Black()
                     });
                     currentInputIdx = 0
                 })
             }
+            //bottom right is "enter" button
             else if (col == 2 && row == 3){
-                buttonImage = new UIImage(panelCanvas, new Texture("images/room4/pwdpanel_enter.png"))
+                //create image for button
+                buttonImage = new UIImage(panelRect, new Texture("images/room4/pwdpanel_enter.png"))
+                //when clicked we check if entered password is correct
                 buttonImage.onClick = new OnClick(event =>{
                     let inputPwd = ""
                     for (let i=0; i<3; i++){
                         inputPwd = inputPwd + panelInputs[i].text.value
                     }
+                    //if password is correct
                     if (inputPwd == password){
                         panelInputs[0].text.value = "O"
                         panelInputs[0].text.color = Color4.Green()
@@ -68,7 +107,9 @@ export function CreateRoom4() : void{
                         panelInputs[1].text.color = Color4.Green()
                         panelInputs[2].text.value = "!"
                         panelInputs[2].text.color = Color4.Green()
+                        //TODO: make something happen
                     }
+                    //if password is incorrect
                     else{
                         panelInputs[0].text.value = "E"
                         panelInputs[0].text.color = Color4.Red()
@@ -80,30 +121,38 @@ export function CreateRoom4() : void{
                     }
                 })
             }
+            //if it is a numerical button
             else{
-                buttonImage = new UIImage(panelCanvas, buttonTexture)
-                numberText = new UIText(panelCanvas)
+                //create image for button
+                buttonImage = new UIImage(panelRect, buttonTexture)
+                //create text for button
+                numberText = new UIText(panelRect)
                 numberText.isPointerBlocker = false
+                //if clicked we set it value to the input boxes
                 buttonImage.onClick = new OnClick(event =>{
+                    //clear inputs if we are entering a new digit and we had and error or other message displayed
                     if (currentInputIdx == 0 && panelInputs[0].text.value != ""){
                         panelInputs.forEach(inputSlot => {
                             inputSlot.text.value = ""
                             inputSlot.text.color = Color4.Black()
                         });
                     }
+                    //if we can add an input we add it
                     if (currentInputIdx < 3){
                         panelInputs[currentInputIdx].text.value = value.toString()
                         currentInputIdx++
                     }
                 })
             }
-            buttonImage.sourceWidth = 32
-            buttonImage.sourceHeight = 32
+            //set image
+            buttonImage.sourceWidth = 64
+            buttonImage.sourceHeight = 64
             buttonImage.width = buttonSize.x
             buttonImage.height = buttonSize.y
             buttonImage.positionX = panelPosition.x + col * (buttonSpace.x + buttonSize.x)
             buttonImage.positionY = panelPosition.y - row * (buttonSpace.y + buttonSize.y)
 
+            //if its a numerical button we set up it's text
             if (numberText != null){
                 numberText.width = buttonImage.width
                 numberText.height = buttonImage.height
@@ -111,20 +160,23 @@ export function CreateRoom4() : void{
                 numberText.positionY = buttonImage.positionY
                 numberText.fontAutoSize = true
                 numberText.hTextAlign = "center"
+                numberText.isPointerBlocker = false
                 numberText.value = value.toString()
             }
         }
     }
 
+    //create the input boxes to show the digits entered by the user
     let panelInputs: {image: UIImage, text: UIText}[] = []
     for (let i=0; i<3; i++){
-        let inputSlot = {image: new UIImage(panelCanvas, inputTexture), text: new UIText(panelCanvas)}
-        inputSlot.image.sourceWidth = 32
-        inputSlot.image.sourceHeight = 32
+        let inputSlot = {image: new UIImage(panelRect, inputTexture), text: new UIText(panelRect)}
+        inputSlot.image.sourceWidth = 64
+        inputSlot.image.sourceHeight = 64
         inputSlot.image.width = inputSlot.text.width = buttonSize.x
         inputSlot.image.height = inputSlot.text.height = buttonSize.y
-        inputSlot.image.positionX = inputSlot.text.positionX = panelPosition.x + i * (buttonSpace.x + buttonSize.x)
-        inputSlot.image.positionY = inputSlot.text.positionY = panelPosition.y + (buttonSpace.y + buttonSize.y)
+        inputSlot.image.positionX = inputSlot.text.positionX = i * (buttonSpace.x + buttonSize.x)
+        inputSlot.image.positionY = inputSlot.text.positionY = 65
+        inputSlot.image.isPointerBlocker = inputSlot.text.isPointerBlocker = false
         inputSlot.text.fontAutoSize = true
         inputSlot.text.hTextAlign = "center"
         inputSlot.text.value = ""
@@ -132,9 +184,10 @@ export function CreateRoom4() : void{
         panelInputs.push(inputSlot)
     }
 
+    //create a carpet on the ground
     const carpet = new Entity()
     carpet.addComponent(new GLTFShape("models/room4/carpet.glb"))
-    carpet.addComponent(new Transform({position: new Vector3(3,0,3)}))
+    carpet.addComponent(new Transform({position: new Vector3(19,0,11)}))
     carpet.addComponent(new ToggleComponent(ToggleComponent.ToggleState.Off, value=>{
         if (value == ToggleComponent.ToggleState.On){
             carpet.addComponent(new RotateTransformComponent(carpet.getComponent(Transform).rotation, Quaternion.Euler(0,45,0), 0.7))
@@ -148,68 +201,92 @@ export function CreateRoom4() : void{
     }))
     engine.addEntity(carpet)
 
+    //create a coin under the carpet
     const coin = new Entity()
     coin.addComponent(new GLTFShape("models/room4/coin.glb"))
-    coin.addComponent(new Transform({position: new Vector3(2,0,2.5)}))
+    coin.addComponent(new Transform({position: new Vector3(18,0,10.5)}))
+    coin.addComponent(new OnClick(event =>{
+        coinHintRect.visible = true
+        uiCanvas.visible = true
+    }))
     engine.addEntity(coin)
 
-    const coinHintCanvas = new UICanvas()
-    coinHintCanvas.visible = false
-    const coinHintImage = new UIImage(coinHintCanvas, new Texture("images/room4/coinHint.png"))
-    coinHintImage.sourceWidth = 64
-    coinHintImage.sourceHeight = 64
-    coinHintImage.width = 128
-    coinHintImage.height = 128
+    //create hint ui image to show when coin is clicked
+    const coinHintRect = new UIContainerRect(uiCanvas)
+    coinHintRect.width = "100%"
+    coinHintRect.height = "100%"
+    coinHintRect.visible = false
+    const coinHintImage = new UIImage(coinHintRect, new Texture("images/room4/coinHint.png"))
+    coinHintImage.sourceWidth = 392
+    coinHintImage.sourceHeight = 512
+    coinHintImage.width = 261
+    coinHintImage.height = 341
 
-    const coinHintClose = new UIImage(coinHintCanvas, closeTexture)
+    //create "close" button for the coin's hint ui
+    const coinHintClose = new UIImage(coinHintRect, closeTexture)
     coinHintClose.sourceWidth = 32
     coinHintClose.sourceHeight = 32
     coinHintClose.width = 32
     coinHintClose.height = 32
-    coinHintClose.positionX = 100
-    coinHintClose.positionY = 100
+    coinHintClose.positionX = 196 - 32
+    coinHintClose.positionY = 256 - 32
     coinHintClose.onClick = new OnClick(event =>{
-        coinHintCanvas.visible = false
+        coinHintRect.visible = false
+        uiCanvas.visible = false
     })
 
+    //create a painting on the wall
     const painting = new Entity()
     painting.addComponent(new PlaneShape())
-    painting.addComponent(new Transform({position: new Vector3(2, 1.5, 4), scale: new Vector3(0.7,1,1), rotation: Quaternion.Euler(0,180,0)}))
+    painting.addComponent(new Transform({position: new Vector3(18, 1.5, 13), scale: new Vector3(0.7,1,1), rotation: Quaternion.Euler(0,0,180)}))
     let paintingMat = new Material()
     paintingMat.albedoTexture = new Texture("images/room4/fernpicture.png", {hasAlpha: true})
     paintingMat.hasAlpha = true
-    paintingMat.transparencyMode = 3
     painting.addComponent(paintingMat)
     painting.addComponent(new OnClick(event =>{
-        paintingHintCanvas.visible = true
+        paintingHintRect.visible = true
+        uiCanvas.visible = true
     }))
     engine.addEntity(painting)
 
-    const paintingHintCanvas = new UICanvas()
-    paintingHintCanvas.visible = false
-    const paintingHintImage = new UIImage(paintingHintCanvas, new Texture("images/room4/fernpictureHint.png"))
+    //create hint ui image to show when painting is clicked
+    const paintingHintRect = new UIContainerRect(uiCanvas)
+    paintingHintRect.width = "100%"
+    paintingHintRect.height = "100%"
+    paintingHintRect.visible = false
+    const paintingHintImage = new UIImage(paintingHintRect, new Texture("images/room4/fernpictureHint.png"))
     paintingHintImage.sourceWidth = 392
     paintingHintImage.sourceHeight = 512
-    paintingHintImage.width = 98
-    paintingHintImage.height = 128
+    paintingHintImage.width = 261
+    paintingHintImage.height = 341
 
-    const paintingHintClose = new UIImage(paintingHintCanvas, closeTexture)
+    //create "close" button for the painting's hint ui
+    const paintingHintClose = new UIImage(paintingHintRect, closeTexture)
     paintingHintClose.sourceWidth = 32
     paintingHintClose.sourceHeight = 32
     paintingHintClose.width = 32
     paintingHintClose.height = 32
-    paintingHintClose.positionX = 100
-    paintingHintClose.positionY = 100
+    paintingHintClose.positionX = 196 - 32
+    paintingHintClose.positionY = 256 - 32
     paintingHintClose.onClick = new OnClick(event =>{
-        paintingHintCanvas.visible = false
+        paintingHintRect.visible = false
+        uiCanvas.visible = false
     })
 
-    const button = new Entity()
-    button.addComponent(new GLTFShape("models/generic/redbutton.gltf"))
-    button.addComponent(new Transform({position: new Vector3(3.5,0.2,4.5), scale: new Vector3(0.3,0.3,0.3)}))
-    button.addComponent(new OnClick(event =>{
-        panelCanvas.visible = true
+    //create the numpad lock
+    const numPadLock = new Entity()
+    numPadLock.addComponent(new GLTFShape("models/room4/codePad.glb"))
+    numPadLock.addComponent(new Transform({position: new Vector3(19.5,1.5,13)}))
+    numPadLock.addComponent(new OnClick(event =>{
+        panelRect.visible = true
+        uiCanvas.visible = true
     }))
-    engine.addEntity(button)
+    engine.addEntity(numPadLock)
+
+    //create a temporal wall
+    const tempWall = new Entity()
+    tempWall.addComponent(new PlaneShape())
+    tempWall.addComponent(new Transform({position: new Vector3(19, 1.4, 13.1), scale: new Vector3(4,3,1)}))
+    engine.addEntity(tempWall)
 
 }
